9cba6d1a14817bacf6e46a8b34bd9b548e6a8f07
Manuel Vera <manuelvera202@gmail.com>
2022-11-28 18:10:38 -0300

    Create full game following doc example

---
+++ main.gd
@@ -0,0 +1,62 @@
+extends Node
+
+export (PackedScene) var mob_scene
+var score = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+	
+func new_game():
+	score = 0
+	$HUD.update_score(score)
+	
+	get_tree().call_group("mobs", "queue_free")
+	
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$Music.play()
+	$HUD.show_message("Get Ready")
+	yield($StartTimer, "timeout")
+	$ScoreTimer.start()
+	$MobTimer.start()
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$HUD.update_score(score)
+	
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+func _on_MobTimer_timeout():
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.unit_offset = randf()
+	
+	var mob = mob_scene.instance()
+	add_child(mob)
+	
+	mob.position = mob_spawn_location.position
+	
+	var direction = mob_spawn_location.rotation + PI / 2
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	var velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+
+
+func _on_Player_hit():
+	pass # Replace with function body.
